a92ce0ddbdcdccc8b8b7c40d2eff2c6bdb62c7c3
schwermc <schwermc@my.erau.edu>
2024-03-24 18:24:07 -0700

    Details 1

--- CS_M03_PR01/Assets/Scripts/PlayerCollision.cs
+++ CS_M03_PR01/Assets/Scripts/PlayerCollision.cs
@@ -5,15 +5,20 @@ public class PlayerCollision : MonoBehaviour
     public PlayerMovement movement;
     private GameManager gameManager;
 
+    private Material playerColor;
+    private Color deathColor = new Color(1f, .5f, 1f, .5f);
+
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        playerColor = GetComponent<Renderer>().material;
     }
 
     void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "obstacle")
         {
+            playerColor.SetColor("_Color", deathColor);
             movement.enabled = false;
             gameManager.EndGame();
         }
